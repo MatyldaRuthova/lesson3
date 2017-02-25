4d940ad7d192a4a7de56b0f99af0b8c4535eea3a
Lucie Kordacova <luciekordac@gmail.com>
2017-02-25 17:37:19 +0000

    rows added

--- lesson3.sql
+++ lesson3.sql
@@ -1,3 +1,6 @@
 USE test;
 CREATE TABLE lesson3(id int primary key AUTO_INCREMENT, name text, year int);
 INSERT INTO lesson3(name, year) VALUES ("Anna", 25);
+INSERT INTO lesson3(name, year) VALUES ("Jana", 38);
+INSERT INTO lesson3(name, year) VALUES ("Mariana", 19);
+
