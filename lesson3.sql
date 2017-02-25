44696c5246e03d1c01810a173bc345c84a2b8455
Lucie Kordacova <luciekordac@gmail.com>
2017-02-25 17:43:25 +0000

    without command for mariadb

--- lesson3.sql
+++ lesson3.sql
@@ -1,4 +1,4 @@
-USE test;
+
 CREATE TABLE lesson3(id int primary key AUTO_INCREMENT, name text, year int);
 INSERT INTO lesson3(name, year) VALUES ("Anna", 25);
 INSERT INTO lesson3(name, year) VALUES ("Jana", 38);
