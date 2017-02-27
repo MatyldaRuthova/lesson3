a899b6f6f8598f08b2c55bee18aeea44db08fd41
Marek <nospam.keram@gmail.com>
2017-02-27 22:44:22 +0000

    Deleting empty lines

--- lesson3.sql
+++ lesson3.sql
@@ -3,6 +3,4 @@ CREATE TABLE lesson3(id int primary key AUTO_INCREMENT, name text, year int);
 INSERT INTO lesson3(name, year) VALUES ("Anna", 25);
 INSERT INTO lesson3(name, year) VALUES ("Jana", 38);
 INSERT INTO lesson3(name, year) VALUES ("Mariana", 19);
-
-
 INSERT INTO lesson3(name, year) VALUES ("Jonathan", 29);
